5d2c4c3650b67f7de118ca89a598c208ed61b6d8
tj <tj@bnd.cn>
2019-07-17 11:03:36 +0800

    jk_build.sh Add mtk support

--- jk_build.sh
+++ jk_build.sh
@@ -4,13 +4,21 @@ if [ $# -ne 3 ]; then
 	echo "Bad Value, need 3 args"
 	exit 1
 fi
+
 tag="$2-$3-$(date +%Y%m%d%H%M)"
 echo "Build Info: $1 $2 $3, tag it : $tag"
 git tag $tag
-rm -rf out;
+#rm -rf out;
 . build/envsetup.sh
 lunch  $1 $2 $3
-mall && mup
+if [[ $1 =~ rk ]]; then
+	echo "RK build, use special command"
+	mall && mup
+else
+	echo "MTK or other build, just make only"
+	make -j16
+    $(dirname $0)/mtk_pack
+fi
 if [ $? -eq 0 ]; then
 	echo "Build Finish ^_^"
 else
