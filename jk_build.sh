14b4cc651d67ba3609a5e380ad8d3928a2f54432
tj <tj@bnd.cn>
2019-07-23 20:22:25 +0800

    jk_build.sh add log info to file function

--- jk_build.sh
+++ jk_build.sh
@@ -8,6 +8,19 @@ fi
 tag="$2-$3-$(date +%Y%m%d%H%M)"
 echo "Build Info: $1 $2 $3, tag it : $tag"
 git tag $tag
+
+# we store info in ../pcba/
+fn=${2}-${3}-changelist.txt
+cp ../pcbas/$fn ../pcbas/${fn}.bak                          
+echo $tag > ../pcbas/$fn
+echo "====================================" >> ../pcbas/$fn
+git log --pretty=format:%B ${GIT_PREVIOUS_COMMIT}..${GIT_COMMIT} >> ../pcbas/$fn
+echo >> ../pcbas/$fn
+cat ../pcbas/${fn}.bak >> ../pcbas/$fn
+if [[ $1 = "test" ]]; then
+    echo "Test case just return"
+    exit 0
+fi
 #rm -rf out;
 . build/envsetup.sh
 lunch  $1 $2 $3
