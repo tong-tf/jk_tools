1dc149e9a95f844f0c5a53ca79be69ea6d9f953f
tj <tj@bnd.cn>
2019-09-10 14:08:13 +0800

    add jk_mtk.sh for build origin mtk

---
+++ jk_mtk.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Welcome to JENKINS compile system"
+#rm -rf out;
+. build/envsetup.sh
+lunch  $1
+echo "MTK or other build, just make only"
+make -j24
+if [ $? -eq 0 ]; then
+	echo "Build Finish ^_^"
+else
+	echo "Build Fail @_@"
+fi
+if [ -n "$2" ]; then
+	echo "Build OTA start"
+	make otapackage -j24
+fi
+if [ $? -eq 0 ]; then
+	echo "Build OTA Finish ^_^"
+	$(dirname $0)/mtk_pack OTA
+
+else
+	echo "Build Fail @_@"
+fi
+
+
+
+
