dd2959dca9430473ee20cf135e3c485efb13f69a
tj <tj@bnd.cn>
2019-10-25 09:48:45 +0800

    jk_mtk.sh update

--- jk_mtk.sh
+++ jk_mtk.sh
@@ -3,7 +3,9 @@ echo "Welcome to JENKINS compile system"
 #rm -rf out;
 . build/envsetup.sh
 lunch  $1
+rm -rf out/target/product/tb8176p1_64_bsp/obj/ETC/system_build_prop_intermediates/
 echo "MTK or other build, just make only"
+make installclean;
 make -j24
 if [ $? -eq 0 ]; then
 	echo "Build Finish ^_^"
